f2215f96852f04d9ad63495b171bc7955b93ef4a
PleasedSkin <vincent.froger@gmail.com>
2025-03-22 22:54:50 +0100

    Fix map

--- projet/src/app/app.component.ts
+++ projet/src/app/app.component.ts
@@ -8,12 +8,12 @@ import { from, interval, Observable, of, switchMap, tap } from 'rxjs';
 import { EquipeRessource } from './core/model/equipeRessource';
 import { CommonModule } from '@angular/common';
 import { Application, Assets, Sprite, Text, Texture } from 'pixi.js';
+import { OutlineFilter } from 'pixi-filters';
 import { MondeService } from './shared/services/monde.service';
 import { InfoMap } from './core/model/infoMap';
 import { TileService } from './shared/services/tile.service';
 import { TextureChargee } from './core/model/texture-chargee';
 import { Villageois } from './core/model/villageois';
-import { Equipe } from './core/model/equipe';
 
 @Component({
   selector: 'app-root',
@@ -33,6 +33,45 @@ export class AppComponent implements OnInit {
   villageoisEquipePerso: Villageois[] = [];
   equipes: any[] = [];
 
+  couleurs = [
+    '#561ED3',
+    '#8ABC88',
+    '#A08ADF',
+    '#1C21D8',
+    '#92BC9D',
+    '#771ABA',
+    '#ED2189',
+    '#245B82',
+    '#730F74',
+    '#0ADB5B',
+    '#23E77F',
+    '#348579',
+    '#9C13E0',
+    '#CD6781',
+    '#3F24A0',
+    '#204509'
+  ];
+
+  couleursHex = [
+    0x561ED3,
+    0x8ABC88,
+    0xA08ADF,
+    0x1C21D8,
+    0x92BC9D,
+    0x771ABA,
+    0xED2189,
+    0x245B82,
+    0x730F74,
+    0x0ADB5B,
+    0x23E77F,
+    0x348579,
+    0x9C13E0,
+    0xCD6781,
+    0x3F24A0,
+    0x204509
+  ];
+
+
 
 
   constructor(
@@ -43,8 +82,14 @@ export class AppComponent implements OnInit {
   ) {}
 
     ngOnInit(): void {
+
       this.recupererInfosResources().subscribe();
       this.recupererInfosEquipes().subscribe();
+
+      // setInterval(_ => {
+      //   this.recupererInfosResources().subscribe();
+      //   this.recupererInfosEquipes().subscribe();
+      // }, this.INTERVALLE_REFRESH);
 
       const demandeAction: DemandeAction = {
         action: NomAction.RECOLTER,
@@ -57,7 +102,7 @@ export class AppComponent implements OnInit {
     recupererInfosEquipes() {
       return this.equipeService.recupererEquipes().pipe(
         tap(equipes => {
-          this.equipes = equipes.map(equipe => {return {nom: `${equipe.nom} [${equipe.type}]`, score: equipe?.ressources?.find(res => res.ressource.nom === 'POINT')?.quantite}});
+          this.equipes = equipes.map((equipe, index) => {return {id: equipe.idEquipe, nom: `${equipe.nom} [${equipe.type}]`, color: this.couleurs[index], colorHex: this.couleursHex[index], score: equipe?.ressources?.find(res => res.ressource.nom === 'POINT')?.quantite}});
           this.equipes.sort((a, b) => b.score - a.score);
         })
       );
@@ -96,7 +141,7 @@ export class AppComponent implements OnInit {
     }
 
     afficherMap() {
-      console.log('Affichage de la map');
+      // console.log('Affichage de la map');
       this.villageoisService.recupererListeVillageoisEquipe(NOTRE_ID_EQUIPE).pipe(
         tap(villageoisList => {
           this.villageoisEquipePerso = villageoisList;
@@ -119,7 +164,7 @@ export class AppComponent implements OnInit {
 
     }
 
-    private afficherSprite(infoMap: InfoMap, imagePath: string): Observable<any> {
+    private afficherSprite(infoMap: InfoMap, imagePath: string, outlineNecessaire = false): Observable<any> {
       if (!!imagePath) {
         const texturePreChargee = this.texturesChargees.find(textureChargee => textureChargee.nomImage === imagePath)?.texture;
         const texture$ = !!texturePreChargee ? of(texturePreChargee) : from(Assets.load(imagePath));
@@ -130,12 +175,29 @@ export class AppComponent implements OnInit {
               if (!texturePreChargee) {
                 this.texturesChargees.push({nomImage: imagePath, texture});
               }
-              const sprite = new Sprite({texture});
+              const sprite = new Sprite({texture, interactive: true});
               if (sprite) {
+                if (true) { // outlineNecessaire
+                  const colorHex = this.equipes.find(equipe => equipe.id === infoMap.batiment_construit?.proprietaire?.idEquipe)?.colorHex || 0x000000;
+
+                  sprite.filters = [new OutlineFilter({thickness: colorHex === 0x000000 ? 1 : 2, color: colorHex})];
+
+                  sprite.on('click', () => {
+                    alert(`Coordonnées : ${infoMap.coord_x},${infoMap.coord_y}`);
+                  });
+
+                  sprite.on('mouseover', () => {
+                    sprite.filters = [new OutlineFilter({thickness: 2, color: 0xffffff})];
+                  });
+
+                  sprite.on('mouseout', () => {
+                    sprite.filters = [new OutlineFilter({thickness: 1, color: colorHex})];
+                  });
+                }
                 sprite.height = this.TAILLE_TILE;
                 sprite.width = this.TAILLE_TILE;
                 sprite.x = infoMap.coord_x * this.TAILLE_TILE;
-                sprite.y = infoMap.coord_y * this.TAILLE_TILE;
+                sprite.y = 32 * this.TAILLE_TILE - infoMap.coord_y * this.TAILLE_TILE;
                 this.app.stage.addChild(sprite);
               }
             })
@@ -147,7 +209,7 @@ export class AppComponent implements OnInit {
 
 
     afficherInfoMap(infoMap: InfoMap) {
-      this.afficherSprite(infoMap, this.tileService.determinerTilePourBiome(infoMap.biome)).pipe(
+      this.afficherSprite(infoMap, this.tileService.determinerTilePourBiome(infoMap.biome), true).pipe(
         switchMap(_ => {
           return this.afficherSprite(infoMap, this.tileService.determinerTilePourTerrain(infoMap.terrain));
         }),
