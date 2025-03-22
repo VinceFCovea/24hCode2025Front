98d9795f09c4697d3f5337fc445b9ef3f3bd69bb
Essi Chainer KOMISSA ZOTSU <essikomissa@gmail.com>
2025-03-22 14:22:22 +0100

    Fichiers mocks

---
+++ projet/src/app/mocks/villagers.ts
@@ -0,0 +1,67 @@
+export const villageois = [
+    {
+        "idVillageois": "61acd05a-a8e2-45b9-a757-5d4138c92c63",
+        "nom": "Michel",
+        "dateDerniereAction": "2025-03-22T11:46:33.86775Z",
+        "disponible": true,
+        "type": {
+          "nom": "PEON",
+          "description": "Paysan de base",
+          "mutliplicateurDeCooldown": 1.0
+        },
+        "positionX": 30,
+        "positionY": 28
+    },
+    {
+        "idVillageois": "17e9cdb2-6bb1-484e-ad06-5f49c47e2034",
+        "nom": "Michel",
+        "dateDerniereAction": "2025-03-22T12:48:36.212231Z",
+        "disponible": true,
+        "type": {
+          "nom": "PEON",
+          "description": "Paysan de base",
+          "mutliplicateurDeCooldown": 1.0
+        },
+        "positionX": 29,
+        "positionY": 28
+    },
+    {
+        "idVillageois": "0d53b017-10d0-48a2-afe2-e5a292648e56",
+        "nom": "Michel",
+        "dateDerniereAction": "2025-03-22T11:36:57.850053Z",
+        "disponible": true,
+        "type": {
+          "nom": "PEON",
+          "description": "Paysan de base",
+          "mutliplicateurDeCooldown": 1.0
+        },
+        "positionX": 29,
+        "positionY": 27
+    },
+    {
+        "idVillageois": "c71928dd-5c72-4c49-8c34-18f7301507b9",
+        "nom": "Michel",
+        "dateDerniereAction": null,
+        "disponible": true,
+        "type": {
+          "nom": "PEON",
+          "description": "Paysan de base",
+          "mutliplicateurDeCooldown": 1.0
+        },
+        "positionX": 28,
+        "positionY": 28
+      },
+      {
+        "idVillageois": "1c5040c4-c3f1-408e-a0e6-eec4409e5991",
+        "nom": "Michel",
+        "dateDerniereAction": null,
+        "disponible": true,
+        "type": {
+          "nom": "PEON",
+          "description": "Paysan de base",
+          "mutliplicateurDeCooldown": 1.0
+        },
+        "positionX": 28,
+        "positionY": 28
+      }
+]
